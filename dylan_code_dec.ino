0d1a5541269b8b76550eddcce2396b7982f2f165
1leesdyl <34376399+1leesdyl@users.noreply.github.com>
2017-12-08 10:14:48 -0500

    Upload code
    First commit, testing servo code.  Wrote a function to move the servo along and angle in a increments that you chose.  Trying to sweep the servo using this function, not sure how moving backwards will work; negative numbers were used.

---
+++ dylan_code_dec.ino
@@ -0,0 +1,46 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+#define SERVO1_PIN 9
+#define SERVO2_PIN 10
+#define RX 3
+#define TX 2
+
+SoftwareSerial mySerial(TX, RX);
+String fromAndroid = "";
+
+int pwm3 = 3;
+
+Servo servo1;
+int servo1_pos = 0;
+Servo servo2;
+int servo2_pos = 0; 
+
+boolean stringComplete = false;  
+
+// Move servo along angle
+void servo_angle(Servo myservo, int mypos, int angle, int speed = 1) {
+  for (mypos = 0; mypos <= angle; mypos += speed) { 
+    myservo.write(mypos);              
+    delay(15);                       
+  }
+}
+
+// Move servo along angle and return
+/*
+void sweep_angle(Servo myservo, int mypos, int v = 1, int angle = 180) {
+  servo_angle
+}
+*/
+
+
+void setup() {
+  servo1.attach(SERVO1_PIN);
+  servo2.attach(SERVO2_PIN);
+}
+
+void loop() {  
+  servo_angle(servo1, servo1_pos, 180);
+  servo_angle(servo1, servo1_pos, -180, -1);
+}
+
