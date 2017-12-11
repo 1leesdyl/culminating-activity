7e6b49d59f25a406a02f1040727a40487e48c1bf
1leesdyl <34376399+1leesdyl@users.noreply.github.com>
2017-12-11 10:21:18 -0500

    After servo library research
    After further research, the servo knows which angle it is at.  Therefore the math inside the for loop is wrong.  This commmit hopes to fix these issues.

--- dylan_code_dec.ino
+++ dylan_code_dec.ino
@@ -1,15 +1,7 @@
-#include <SoftwareSerial.h>
 #include <Servo.h>
 
 #define SERVO1_PIN 9
 #define SERVO2_PIN 10
-#define RX 3
-#define TX 2
-
-SoftwareSerial mySerial(TX, RX);
-String fromAndroid = "";
-
-int pwm3 = 3;
 
 Servo servo1;
 int servo1_pos = 0;
@@ -19,28 +11,20 @@ int servo2_pos = 0;
 boolean stringComplete = false;  
 
 // Move servo along angle
-void servo_angle(Servo myservo, int mypos, int angle, int speed = 1) {
-  for (mypos = 0; mypos <= angle; mypos += speed) { 
+void servo_angle(Servo myservo, int mypos, int angle, int inc) {
+  angle -= mypos;
+  for (int pos = mypos; pos <= angle; mypos += inc) { 
     myservo.write(mypos);              
     delay(15);                       
   }
 }
 
-// Move servo along angle and return
-/*
-void sweep_angle(Servo myservo, int mypos, int v = 1, int angle = 180) {
-  servo_angle
-}
-*/
-
-
 void setup() {
   servo1.attach(SERVO1_PIN);
   servo2.attach(SERVO2_PIN);
 }
-
+p
 void loop() {  
-  servo_angle(servo1, servo1_pos, 180);
-  servo_angle(servo1, servo1_pos, -180, -1);
+  servo_angle(servo1, servo1_pos, 180, 5);
 }
 
