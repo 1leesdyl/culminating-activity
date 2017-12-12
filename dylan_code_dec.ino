0fdba88fa19814d1b396fa710c8327ccc1fa83ef
1leesdyl <34376399+1leesdyl@users.noreply.github.com>
2017-12-12 10:39:06 -0500

    Forward function for servos
    Getting two servos to work together did not work, therefore we are going back to basics, and iterating on a list of servos.

--- dylan_code_dec.ino
+++ dylan_code_dec.ino
@@ -2,26 +2,34 @@
 
 #define SERVO1_PIN 9
 #define SERVO2_PIN 10
+#define SERVO3_PIN 10
 
 Servo servo1;
 int servo1_pos = 0;
 Servo servo2;
-int servo2_pos = 0; 
+int servo2_pos = 0;
+Servo servo3;
+int servo3_pos = 0; 
+
+Servo myservos[] = {servo1, servo2, servo3};
+
+void forward(Servo servo) {
+  servo.write(0);
+  delay(700);
+  servo.write(180);
+  delay(700);
+}
 
 void setup() {
   servo1.attach(SERVO1_PIN);
   servo2.attach(SERVO2_PIN);
+  servo3.attach(SERVO2_PIN);
 }
 
-void loop() {  
-  for(servo1_pos = 0; servo1_pos < 180; servo1_pos += 1)
-  {
-    for(servo2_pos = 180; servo2_pos > 0; servo2_pos -= 1)
-    { 
-      servo1.write(servo1_pos);
-      servo2.write(servo2_pos);
-      delay(4);
-    }
-  }
+void loop() {
+  int i;
+  for (i = 0; i < 3; i += 1) {
+    forward(myservos[i]);
+  }  
 }
 
