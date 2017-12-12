9011819b3b0d1e49e3aec6700fc8d6e44e029338
1leesdyl <34376399+1leesdyl@users.noreply.github.com>
2017-12-12 10:07:59 -0500

    Two servos at once
    Scrapped function to iterate on one servo individually since it was redundant because servos can control themselves. Replaced with some testing code for getting two servos to sweep at once.

--- dylan_code_dec.ino
+++ dylan_code_dec.ino
@@ -8,23 +8,20 @@ int servo1_pos = 0;
 Servo servo2;
 int servo2_pos = 0; 
 
-boolean stringComplete = false;  
-
-// Move servo along angle
-void servo_angle(Servo myservo, int mypos, int angle, int inc) {
-  angle -= mypos;
-  for (int pos = mypos; pos <= angle; mypos += inc) { 
-    myservo.write(mypos);              
-    delay(15);                       
-  }
-}
-
 void setup() {
   servo1.attach(SERVO1_PIN);
   servo2.attach(SERVO2_PIN);
 }
-p
+
 void loop() {  
-  servo_angle(servo1, servo1_pos, 180, 5);
+  for(servo1_pos = 0; servo1_pos < 180; servo1_pos += 1)
+  {
+    for(servo2_pos = 180; servo2_pos > 0; servo2_pos -= 1)
+    { 
+      servo1.write(servo1_pos);
+      servo2.write(servo2_pos);
+      delay(4);
+    }
+  }
 }
 
