e5fbf12cbc09e7d2dd0714e642dd0cadfeaa994b
1leesdyl <34376399+1leesdyl@users.noreply.github.com>
2018-01-12 10:39:56 -0500

    EEPROM revolution
    EEPROM is being used to store the password of the user:
    Lots of EEPROM development and a reset script.
    TODO: Read input as a string then cast it to a char array and store it inside the EEPROM

--- dylan_code_dec.ino
+++ dylan_code_dec.ino
@@ -1,35 +1,108 @@
-#include <Servo.h>
+#include <Servo.h> // library for servos
+#include <SoftwareSerial.h> // TX RX software library for bluetooth
+#include <EEPROM.h> // library for reading and writing to the EEPROM
+#include <String.h> // library for manipulating strings
 
+// constant definitions
 #define SERVO1_PIN 9
 #define SERVO2_PIN 10
 #define SERVO3_PIN 10
+#define BLUE_RX 6
+#define BLUE_TX 5
+#define ZERO 0
+#define SZ 1024
+#define KEY 123
+#define PASS_LEN 20
 
-Servo servo1;
-int servo1_pos = 0;
-Servo servo2;
-int servo2_pos = 0;
-Servo servo3;
-int servo3_pos = 0; 
+// init bluetooth using pin constants for tx and rx
+SoftwareSerial bluetooth(BLUE_TX, BLUE_RX);
 
-Servo myservos[] = {servo1, servo2, servo3};
+// init servos
+Servo servo_lock;
+Servo servo_unlock;
 
-void forward(Servo servo) {
-  servo.write(0);
-  delay(700);
-  servo.write(180);
-  delay(700);
-}
+int lservo;
+int uservo;
+
+// char array for reading values from EEPROM
+char data[SZ];
 
 void setup() {
-  servo1.attach(SERVO1_PIN);
-  servo2.attach(SERVO2_PIN);
-  servo3.attach(SERVO2_PIN);
+  // attach servos
+  servo_lock.attach(SERVO1_PIN);
+  servo_unlock.attach(SERVO2_PIN);
+  
+  // Setup usb serial connection to computer with 9600 baud rate
+  Serial.begin(9600);
+  
+  while (!Serial) 
+  {
+    ; // wait for serial port to connect.
+  }
+
+  //Setup Bluetooth serial connection to android with 9600 baud rate
+  bluetooth.begin(9600);
+
+  //reset servo positions to neutral positions
+  servo_lock.write(0);
+  delay(700);
+  servo_unlock.write(90);
+  delay(700);
+
+  // Get size of EEPROM for debug
+  Serial.print("Size of EEPROM: ");
+  Serial.print(EEPROM.length(), DEC);
+  Serial.print(" bytes.");
+  Serial.println();
+
+  // read all data from EEPROM
+  for (int address = 0; address <= (SZ-1); ++address) { data[address] = EEPROM.read(address); }
+
+  // check if first time loading
+  if (data[SZ-1] == KEY) 
+  {
+    Serial.println("Device has already been setup!");
+    return 0;
+  }
+  // it is first time setting up
+  else 
+  {
+    char password[PASS_LEN];
+    
+    Serial.println("First time setup!");
+    for (int addr = 0; addr <= (SZ-1); ++addr) 
+    {
+      EEPROM.write(addr, ZERO);
+    }
+    EEPROM.write(SZ-1, KEY);
+
+    Serial.println("Please enter a password:");
+
+    while (Serial.available())
+    {
+    Serial.readString().toCharArray(password, PASS_LEN);
+    }
+    
+    for (int i = 0; i < sizeof(password); ++i)
+    {
+      Serial.print(password[i]);
+    }
+    Serial.println();
+  }
 }
 
 void loop() {
-  int i;
-  for (i = 0; i < 3; i += 1) {
-    forward(myservos[i]);
-  }  
+  
+  
+  //Read from bluetooth and write pos (number) to servo
+  if (bluetooth.available() > 0) // receive number from bluetooth (zero is no data / not available)
+  {
+    lservo = bluetooth.read(); // save the received number to servopos
+    Serial.println(lservo); // serial print servopos current number received from bluetooth (use serial monitor)
+    servo_lock.write(lservo); // rotate the servo the angle received from the android app
+  }
+
 }
+
+
 
---
+++ reset.ino
@@ -0,0 +1,31 @@
+#include <EEPROM.h>
+
+#define SZ 1024
+#define ZERO 0
+#define KEY 123
+
+void setup() {
+  // run once:
+  // Setup usb serial connection to computer with 9600 baud rate
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  
+  for (int addr = 0; addr <= (SZ-1); ++addr) {
+    EEPROM.write(addr, ZERO);
+  }
+
+  //EEPROM.write(1023, KEY);
+  
+  for (int address = 0; address <= (SZ-1); ++address)
+  {    
+    Serial.print(address);
+    Serial.print("\t");
+    Serial.print(EEPROM.read(address), DEC);
+    Serial.println();
+  }
+}
+
+void loop() { /* not needed */ }
